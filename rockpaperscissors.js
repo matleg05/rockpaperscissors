1af2e5fe8157543f10cad862410894b8fce01384
matleg <mathieu.leger05@gmail.com>
2022-11-09 15:44:56 +0100

    add computer choice

---
+++ rockpaperscissors.js
@@ -0,0 +1,13 @@
+function getComputerChoice() {
+    let randomNumber = Math.ceil(Math.random() * 3)
+        if (randomNumber === 1) {
+            return "rock";
+        } else if (randomNumber === 2) {
+            return "paper";
+        } else if (randomNumber === 3) {
+            return "scissors";
+        }
+    
+}
+
+console.log(getComputerChoice())
